32f6f17c49acf69b2849c301fbf588e1458c66f1
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2025-04-16 14:33:12 +0545

    feat:save course

--- src/main/java/com/example/demo/service/CourseServiceImpl.java
+++ src/main/java/com/example/demo/service/CourseServiceImpl.java
@@ -1,23 +1,44 @@
 package com.example.demo.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.example.demo.model.Course;
+import com.example.demo.model.Student;
 import com.example.demo.repo.CourseRepo;
+import com.example.demo.repo.StudentRepo;
 
 @Service
 public class CourseServiceImpl  implements CourseService{
 
     private final CourseRepo courseRepo;
 
-    public CourseServiceImpl(CourseRepo courseRepo){
+    private final StudentRepo repo;
+
+    public CourseServiceImpl(CourseRepo courseRepo,StudentRepo repo){
         this.courseRepo=courseRepo;
+
+        this.repo=repo;
 
     }
 
     @Override
     public void saveCourse(Course course) {
-      
+ 
+
+      for(Student li:course.getStudentList()){
+          course.getStudentList().add(li);
+
+          li.getCourseList().add(course);
+
+         repo.save(li);
+      }
+
+      courseRepo.save(course);
+
+
     }
 
 }
