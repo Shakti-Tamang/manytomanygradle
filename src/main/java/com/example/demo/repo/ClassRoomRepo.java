3d223dfc90b4a5a53f4e4e3f79d0e4f9ad4655e7
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2025-04-18 07:32:53 +0545

    feat:added course cutom repo method

--- src/main/java/com/example/demo/repo/ClassRoomRepo.java
+++ src/main/java/com/example/demo/repo/ClassRoomRepo.java
@@ -1,11 +1,18 @@
 package com.example.demo.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.ClassRoom;
 
 @Repository
 public interface ClassRoomRepo extends JpaRepository<ClassRoom, Long> {
+
+    @Query("SELECT c FROM ClassRoom c"+"JOIN FETCH c.students")
+
+    public List<ClassRoom> findAllClasssRoomStudent();
 
 }
