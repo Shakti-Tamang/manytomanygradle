52e9e8251d09e5c72de9aba923f261cd251caa24
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2025-04-26 22:47:11 +0545

    feat:graphql implementation

--- src/main/java/com/example/demo/model/Players.java
+++ src/main/java/com/example/demo/model/Players.java
@@ -1,12 +1,25 @@
 package com.example.demo.model;
 
 import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 
-
+@Entity
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Players {
 
     @Id
@@ -18,7 +31,20 @@ public class Players {
     private String name;
 
 
+
+    @Column(unique = true,nullable = false)
+    @Enumerated(EnumType.STRING)
     private Team team;
+
+    private int age;
+
+    private String address;
+
+    private float height;
+
+
+    @Email
+    private String email;
 
 
 }
---
+++ src/main/java/com/example/demo/service/PlyerServiceImpl.java
@@ -0,0 +1,79 @@
+package com.example.demo.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Players;
+import com.example.demo.repo.PlayerRepo;
+
+
+
+@Service
+public class PlyerServiceImpl implements PlyerService {
+
+    private final PlayerRepo playerRepo;
+
+    public  PlyerServiceImpl(PlayerRepo playerRepo){
+        this.playerRepo=playerRepo;
+
+    }
+
+    @Override
+    public void savePlayer(Players players) {
+
+        try{
+     
+    Long id=Long.parseLong(UUID.randomUUID().toString());
+    
+    // String a="";
+// System.out.println(a.compareTo("skjdksd"));
+
+players.setId(id);
+playerRepo.save(players); 
+
+        }
+
+        catch(Exception ex){
+
+            throw new RuntimeException("error"+ex.getMessage());
+
+        }
+
+    }
+    
+
+
+    @QueryMapping("getPlayers")
+
+    @Override
+    public List<Players> getAllPlayers() {
+      
+        List<Players>list=playerRepo.findAll();
+
+        return list.isEmpty()? new ArrayList<>():list;
+    }
+
+    @QueryMapping("getPlayers")
+    @Override
+    public Players getPlayerById(@Argument int id) {
+        Players player=playerRepo.findById(id).orElse(null);
+
+int a=4;
+int b=90;         
+
+        String result=a>b?"djfjdf":"dhdffd";
+        return player;
+      
+    }
+    
+
+    }
+
+
+
+
