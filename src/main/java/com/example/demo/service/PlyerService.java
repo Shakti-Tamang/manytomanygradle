b53f1c69d05f2e1800cfa76731b85f5d1ce2c968
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2025-04-28 12:14:51 +0545

    feat:mutation

--- src/main/java/com/example/demo/service/PlyerService.java
+++ src/main/java/com/example/demo/service/PlyerService.java
@@ -2,13 +2,14 @@ package com.example.demo.service;
 
 import java.util.List;
 
+import com.example.demo.GraphqlDto.playerInput;
 import com.example.demo.model.Players;
 
 
 
 public interface PlyerService {
 
-    public void savePlayer(Players players);
+    public void savePlayer(playerInput players);
 
     public List<Players>getAllPlayers();
 
