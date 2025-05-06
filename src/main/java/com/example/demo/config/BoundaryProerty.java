4d9b4049839c303896ffbe618c81f4e21653edf8
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2025-05-06 17:18:19 +0545

    feat:boundaryProperty

--- src/main/java/com/example/demo/config/BoundaryProerty.java
+++ src/main/java/com/example/demo/config/BoundaryProerty.java
@@ -1,5 +1,6 @@
 package com.example.demo.config;
 
+import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
 public class BoundaryProerty {
@@ -7,5 +8,31 @@ public class BoundaryProerty {
     private String name;
 
     private PropertyChangeSupport support=new PropertyChangeSupport(this);
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String newName){
+   String oldName=this.name;
+
+    this.name=newName;   
+   support.firePropertyChange("shakti", oldName, newName);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener  listener){
+support.addPropertyChangeListener(listener);
+    }
+
+    public static void main(String[] args) {
+        BoundaryProerty boundaryProerty=new BoundaryProerty();
+
+        boundaryProerty.addPropertyChangeListener(event->{System.out.println("Name changed from"+event.getOldValue()+"to"+event.getNewValue());});
+
+        boundaryProerty.setName("shakti");
+        boundaryProerty.setName("amu");
+    }
+
+
 
 }
